05592487a315684d0f12a162a113a80072917c35
Katy Kate <krawczykate@gmail.com>
2017-10-19 18:17:18 +0100

    5.1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
 	get 'orders/index'
 
-  root 'static_pages#landing_page'
+  root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders, only: [:index, :show, :create, :destroy]

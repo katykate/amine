3aca91439becdab5a58ca69b6f21d69936d4ac46
Katy Kate <krawczykate@gmail.com>
2017-10-24 13:42:41 +0100

    5.3 sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
 	get 'orders/index'
 
+	post 'static_pages/thank_you'
+
   root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

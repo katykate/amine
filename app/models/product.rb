0fa0f3df14603416fb08fa508ba8130fce35d3e0
Katy Kate <krawczykate@gmail.com>
2017-10-26 11:57:54 +0100

    ilike

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 	def self.search(search_term)
-		if Rails.env.development?
+		if Rails.env.ENVIRONMENT?
 	  	Product.where("LOWER(name) LIKE ?", "%#{name.downcase}%").take
 	  else
 	  	Product.where("LOWER(name) ILIKE ?", "%#{name}%").take

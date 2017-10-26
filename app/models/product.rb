4abfb83c7fe6161f282f1050d740eb0224b56917
Katy Kate <krawczykate@gmail.com>
2017-10-26 12:33:52 +0100

    like updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
 	def self.search(search_term)
 		if Rails.env.development?
-	  	Product.where("LOWER(name) LIKE ?", "%#{search_term}%").take
+	  	Product.where("name LIKE ?", "%#{search_term}%").take
 	  else
-	  	Product.where("LOWER(name) ilike ?", "%#{search_term}%").take
+	  	Product.where("name ilike ?", "%#{search_term}%").take
 		end
 	end
 end

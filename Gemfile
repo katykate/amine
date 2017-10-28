4796012d08432d063d363d58ffeaa32423d18522
Katy Kate <krawczykate@gmail.com>
2017-10-28 20:38:06 +0100

    5.3 email form

--- Gemfile
+++ Gemfile
@@ -9,9 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
 # Use sqlite3 as the database for Active Record
-group :development, :test do
-  gem 'sqlite3'
-end
+
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -44,6 +42,7 @@ end
 gem 'nodejs-rails', '~> 0.0.1'
 gem 'webpacker', '~> 3.0'
 group :development, :test do
+  gem 'sqlite3'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,13 +9,13 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'kataliento@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
   end
 
   def landing_page

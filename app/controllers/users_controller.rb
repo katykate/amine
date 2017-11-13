acd197f544d351fe8eb12eda577773e6ef68efcd
Katy Kate <krawczykate@gmail.com>
2017-11-13 20:52:35 +0000

    5.11 revision updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
-  before_action :authenticate_user!, except: [:show, :index]
+  
   
   # GET /users
   # GET /users.json

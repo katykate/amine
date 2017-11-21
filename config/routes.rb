b1d16574f49fb18904f9326b33f41cbedd28fdc8
Katy Kate <krawczykate@gmail.com>
2017-11-21 12:27:31 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
 
   get  'thanks', to: 'payments#create', as: 'thanks'
 
-  root 'static_pages#landing_page'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 

1e3804d430e61b17531da997c83f64a69c63f2d4
Katy Kate <krawczykate@gmail.com>
2017-11-21 17:28:40 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require cable
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
--- spec/jobs/comment_update_job_spec.rb
+++ spec/jobs/comment_update_job_spec.rb
@@ -1,16 +1,5 @@
-class CommentUpdateJob < ApplicationJob
-  queue_as :default
-
-  # def perform(*args)
-  def perform(comment, current_user)
-    # Do something later
-    ProductChannel.broadcast_to (comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)
-  end
-
-  private
-
-  def render_comment(comment, current_user)
-    CommentsController.render(partial: 'comments/comment',
-    locals: { comment: comment, current_user: current_user})
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe CommentUpdateJob, type: :job do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
